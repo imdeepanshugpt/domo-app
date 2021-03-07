0957b6dfca32e3d29bf3e5efb146245caaac8c59
imdeepanshugpt <deepanshu.gupta@mtxb2b.com>
2021-03-07 19:36:12 +0530

    React domo app base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as React from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+ 
+// The wrapper exports only a default component class that at the same time is a
+// namespace for the related Props interface (HighchartsReact.Props). All other
+// interfaces like Options come from the Highcharts module itself.
+ 
+const options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        type: 'line',
+        data: [1, 2, 3]
+    }]
 }
+ 
+// React supports function components as a simple way to write components that
+// only contain a render method without any state (the App component in this
+// example).
+ 
+const App = (props) => <div>
+    <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        {...props}
+    />
+</div>
+ 
 
 export default App;
